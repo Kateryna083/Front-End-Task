d8c95740f85237308b607580576aeab305f19ddb
Kateryna083 <159190252+Kateryna083@users.noreply.github.com>
2025-07-21 17:24:47 +0300

    accordion

--- js/main.js
+++ js/main.js
@@ -24,3 +24,18 @@ const swiper = new Swiper(".swiper", {
     onlyInViewport: true,
   },
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
