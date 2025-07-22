495d9ae2975285211ce186117b85b4deca889206
Kateryna083 <159190252+Kateryna083@users.noreply.github.com>
2025-07-22 21:00:27 +0300

    contact-section

--- js/main.js
+++ js/main.js
@@ -10,14 +10,13 @@
 const swiper = new Swiper(".swiper", {
   slidesPerView: 1,
   spaceBetween: 30,
-  loop: true, // Якщо хочеш, щоб слайди крутились по колу
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   pagination: {
     el: ".swiper-pagination",
-    type: "fraction", // або "bullets", "progressbar"
+    type: "fraction",
   },
   keyboard: {
     enabled: true,
